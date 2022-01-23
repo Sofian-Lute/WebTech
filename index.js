09a84aaf777ca208c9bcb7433d8be88476e2a2b1
YusufPekr <yusuf_peker@live.nl>
2022-01-23 23:55:02 +0100

    fixed second table and table headers

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const BASE_URL = "https://wt.ops.labs.vu.nl/api22/ca8317f0";
 
 let phones = [];
+let products = [{
+    type: "Charger",
+    product: "Original OnePlus USB C charger",
+    price: 8,
+  },
+  {
+    type: "Case",
+    product: "Iphone X Black case",
+    price: 24.69,
+  },
+  {
+    type: "Protector",
+    product: "Sapphire for screen",
+    price: 14.25,
+  }
+];
+
+refreshProducts();
 fetchData();
 
 $("#btnSubmit").on("click", (e) => {
@@ -15,13 +33,20 @@ $("#btnReset").on("click", (e) => {
     })
 });
 
+//Click-listeners for main table (phones table)
 $("#btnBrand").on("click", () => sortTable("brand"));
 $("#btnModel").on("click", () => sortTable("model"));
 $("#btnOs").on("click", () => sortTable("os"));
 $("#btnScreensize").on("click", () => sortTable("screensize"));
 $("#btnImage").on("click", () => sortTable("image"));
 
-function sortTable(type){
+//Click-listeners for second table (product table)
+$("#btnType").on("click", () => sortTable("type", true));
+$("#btnProduct").on("click", () => sortTable("product", true));
+$("#btnPrice").on("click", () => sortTable("price", true));
+
+//SORTER of both tables
+function sortTable(type, isProduct = false){
     switch(type){
         case "brand":
             phones.sort((a, b) => ascending(a.brand, b.brand));
@@ -35,13 +60,24 @@ function sortTable(type){
         case "screensize": 
             phones.sort((a, b) => ascending(a.screensize, b.screensize));
             break;
+        case "type":
+            products.sort((a, b) => ascending(a.type, b.type));
+            break;
+        case "product":
+            products.sort((a, b) => ascending(a.product, b.product));
+            break;
+        case "price":
+            products.sort((a, b) => ascending(a.price, b.price));
+            break;
         default:
             phones.sort((a, b) => ascending(a.brand, b.brand));
+            products.sort((a, b) => ascending(a.type, b.type));
             break;
     }
-    refreshTable();
+    if (!isProduct) refreshTable();
+    else refreshProducts();
 }
-
+    
 function ascending(a, b){
     return a > b ? 1 : -1;
 }
@@ -51,6 +87,14 @@ function refreshTable(){
     phones.forEach(phone => {
         const row = `<tr><td>${phone.brand}</td><td>${phone.model}</td><td>${phone.os}</td><td>${phone.screensize}</td><td><img src="${phone.image}" class="phone-img-table"/></td></tr>`;
         $("#table-phones").append(row);
+    });
+}
+
+function refreshProducts(){
+    $("#table-products tr:not(:first)").remove();
+    products.forEach(product => {
+        const row = `<tr><td>${product.type}</td><td>${product.product}</td><td>${product.price}</td></tr>`;
+        $("#table-products").append(row);
     });
 }
 
