f1ed3c43588e41e5457316fa1ca0175a4362786b
YusufPekr <yusuf_peker@live.nl>
2022-01-23 20:20:29 +0100

    new items addable

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const BASE_URL = "https://wt.ops.labs.vu.nl/api22/ca8317f0";
 let phones = [];
 fetchData();
 
-$("#btnSubmit").on("click", (e)=> {
+$("#btnSubmit").on("click", (e) => {
     e.preventDefault();
     saveNewPhone();
-});
+  });
 
 function refreshTable(){
+    $("#table-phones tr:not(:first)").remove();
+    console.log(phones.length);
     phones.forEach(phone => {
         const row = `<tr><td>${phone.brand}</td><td>${phone.model}</td><td>${phone.os}</td><td>${phone.screensize}</td><td><img src="${phone.image}" class="phone-img-table"/></td></tr>`;
         $("#table-phones").append(row);
@@ -16,16 +18,17 @@ function refreshTable(){
 }
 
 function saveNewPhone(){
-    const brand = $("#btnBrand").val();
-    const model = $("#btnModel").val();
-    const os = $("#btnOs").val();
-    const screensize = $("#btnScreensize").val();
-    const image = $("#btnImage").va();
+    const brand = $("#brand").val();
+    const model = $("#model").val();
+    const os = $("#os").val();
+    const screensize = parseFloat($("#screensize").val());
+    const image = $("#image").val();
 
     const phone = {brand, model, os, screensize, image};
 
     $.post(BASE_URL, phone, data => {
-        console.log(data);
+        phones.push(phone)
+        refreshTable();
     });
 }
 
