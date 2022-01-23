c732387e1711e21c5bf390fc9b1b904c2a226e89
YusufPekr <yusuf_peker@live.nl>
2022-01-23 19:49:30 +0100

    fetch function for data from API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const BASE_URL = "https://wt.ops.labs.vu.nl/api22/ca8317f0";
 
+let phones = [];
+
 $("#btnSubmit").on("click", (e)=> {
     e.preventDefault();
     saveNewPhone();
@@ -15,6 +17,12 @@ function saveNewPhone(){
     const phone = {brand, model, os, screensize, image};
 
     $.post(BASE_URL, phone, data => {
+        console.log(data);
+    });
+}
+
+function fetchData(){
+    $.get(BASE_URL, (data) => {
         console.log(data);
     })
 }
