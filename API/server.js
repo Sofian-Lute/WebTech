7d2596c8262af03930c845e2750badce96abee9e
YusufPekr <yusuf_peker@live.nl>
2022-01-27 23:54:05 +0100

    added get post put and delete

--- API/server.js
+++ API/server.js
@@ -16,41 +16,60 @@ app.use(bodyParser.json());
 // This example route responds to http://localhost:3000/hello with an example JSON object.
 // Please test if this works on your own device before you make any changes.
 
-app.get("/hello", function (req, res) {
-  response_body = { Hello: "World" };
+app.get("/api/phones", function (req, res) {
+  db.all(`SELECT * FROM phones `, function (err, rows) {
+    if (err) return res.status(500).json({ error: err });
 
-  // This example returns valid JSON in the response, but does not yet set the
-  // associated HTTP response header.  This you should do yourself in your
-  // own routes!
-  res.json(response_body);
-});
-
-// This route responds to http://localhost:3000/db-example by selecting some data from the
-// database and return it as JSON object.
-// Please test if this works on your own device before you make any changes.
-app.get("/db-example", function (req, res) {
-  // Example SQL statement to select the name of all products from a specific brand
-  db.all(`SELECT * FROM phones WHERE brand=?`, ["Fairphone"], function (
-    err,
-    rows
-  ) {
-    // TODO: add code that checks for errors so you know what went wrong if anything went wrong
-    // TODO: set the appropriate HTTP response headers and HTTP response codes here.
-
-    // # Return db response as JSON
-    return res.json(rows);
+    return res.status(200).json(rows);
   });
 });
 
-app.post("/post-example", function (req, res) {
-  // This is just to check if there is any data posted in the body of the HTTP request:
-  console.log(req.body);
-  return res.json(req.body);
+app.post("/api/phones", function (req, res) {
+  db.all(
+    `INSERT INTO phones (brand, model, os, image, screensize) VALUES (?, ?, ?, ?, ?)`,
+    [
+      req.body.brand,
+      req.body.model,
+      req.body.os,
+      req.body.screensize,
+      req.body.image,
+    ],
+
+    function (err) {
+      if (err) return res.status(500).json({ error: err });
+      return res.status(200).json({ message: "Succesfully added the phone!" });
+    }
+  );
 });
 
-// ###############################################################################
-// This should start the server, after the routes have been defined, at port 3000:
+app.put("/api/phones/:id", function (req, res) {
+  db.all(
+    `UPDATE phones SET brand=?, model=?, os=?, screensize=?, image=? WHERE id=?`,
+    [
+      req.body.brand,
+      req.body.model,
+      req.body.os,
+      req.body.screensize,
+      req.body.image,
+      req.params.id,
+    ],
+    function (err) {
+      if (err) return res.status(500).json({ error: err });
+      return res
+        .status(200)
+        .json({ message: "Succesfully updated the phone!" });
+    }
+  );
+});
 
+app.delete("/api/phones/:id", function (req, res) {
+  db.all(`DELETE FROM phones WHERE id=?`, [req.params.id], function (err) {
+    if (err) return res.status(500).json({ error: err });
+    return res.status(200).json({ message: "Succesfully deleted the phone!" });
+  });
+});
+
+// This should start the server, after the routes have been defined, at port 3000:
 app.listen(3000);
 console.log(
   "Your Web server should be up and running, waiting for requests to come in. Try http://localhost:3000/hello"
